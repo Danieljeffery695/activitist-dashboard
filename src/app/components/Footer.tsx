55f43aa09637c1c20dda4cfc50b813eb5dbe364c
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-09-29 01:59:32 -0700

    Styling footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,42 +1,43 @@
-import Image from "next/image";
+// import Image from "next/image";
+import { cormorant, dancing } from "../layout";
 
 export default function Footer() {
     return (
         // Footer start here
-        <footer className="footer w-full border-t-2 border-t-slate-300 bg-white overflow-hidden relative 320:h-p-900 md:w-p-400">
+        <footer className="footer w-full border-t-2 border-t-slate-300 bg-white overflow-hidden relative 320:h-900 md:h-400">
             <canvas id="footer-canvas" className="footer-canvas w-full h-full absolute"></canvas>
             <div className="w-full h-full flex md:items-center md:justify-between pt-10 320:flex-col md:flex-row lg:justify-center">
                 <div className="h-full pl-3 flex flex-col z-10 md:flex-1">
                     <div className="w-full h-20 flex">
-                        <Image src="" alt="Image" width={100} height={100} />
-                        <h1 className="text-2xl ml-4 font-semibold">XtrCrypto</h1>
+                        <img src="" alt="Image" />
+                        <h1 className={`${cormorant.className} text-2xl ml-4 font-semibold`}>XtrCrypto</h1>
                     </div>
-                    <h3 className="320:text-small-20 text-lg font-bold mb-5">Mailing Address:4th Square Trade Center 180 Greenmile Street, NY 80029</h3>
-                    <h3 className="320:text-small-20 text-lg font-bold mb-5">P: +1 800-256-3312</h3>
-                    <h3 className="320:text-small-20 text-lg font-bold mb-5">Support@xtracypto.com</h3>
+                    <h3 className={`${cormorant.className} 320:text-small-20 text-lg font-bold mb-5`}>Mailing Address:4th Square Trade Center 180 Greenmile Street, NY 80029</h3>
+                    <h3 className={`${cormorant.className} 320:text-small-20 text-lg font-bold mb-5`}>P: +1 800-256-3312</h3>
+                    <h3 className={`${cormorant.className} 320:text-small-20 text-lg font-bold mb-5`}>Support@xtracypto.com</h3>
                 </div>
 
-                <div className="h-full z-10 410:pl-10 md:flex-1 md:flex md:flex-col md:items-center md:pl-0">
-                    <div className="h-20 w-full lg:text-center">
+                <div className="h-full z-10 320:pl-10 410:pl-10 md:flex-1 md:flex md:flex-col md:items-center md:pl-0">
+                    <div className="h-20 w-full md:text-center">
                         <h1 className="text-2xl font-semibold">Links</h1>
                     </div>
                     <ul className="list-outside list-disc">
-                        <li className="320:text-small-20 text-lg mb-3 font-semibold">Services</li>
-                        <li className="320:text-small-20 text-lg mb-3 font-semibold">Investing</li>
-                        <li className="320:text-small-20 text-lg mb-3 font-semibold">Blog</li>
-                        <li className="320:text-small-20 text-lg mb-3 font-semibold">Contact</li>
-                        <li className="320:text-small-20 text-lg mb-3 font-semibold">About</li>
+                        <li className={`${cormorant.className} 320:text-small-20 text-lg mb-3 font-semibold`}>Services</li>
+                        <li className={`${cormorant.className} 320:text-small-20 text-lg mb-3 font-semibold`}>Investing</li>
+                        <li className={`${cormorant.className} 320:text-small-20 text-lg mb-3 font-semibold`}>Blog</li>
+                        <li className={`${cormorant.className} 320:text-small-20 text-lg mb-3 font-semibold`}>Contact</li>
+                        <li className={`${cormorant.className} 320:text-small-20 text-lg mb-3 font-semibold`}>About</li>
                     </ul>
                 </div>
 
-                <div className="h-full z-10 410:pl-10 md:flex-1 lg:flex lg:flex-col lg:items-center md:pl-0">
+                <div className="h-full z-10 320:pl-10 410:pl-10 md:flex-1 lg:flex lg:flex-col lg:items-center md:pl-0">
                     <div className="h-20 w-full lg:text-center">
-                        <h1 className="text-2xl font-semibold">NewsLetter</h1>
+                        <h1 className={`${cormorant.className} text-2xl font-semibold`}>NewsLetter</h1>
                     </div>
-                    <h3 className="text-lg font-semibold">Subscribe to our newsletter</h3>
+                    <h3 className={`${cormorant.className} text-lg font-semibold`}>Subscribe to our newsletter</h3>
                     <form action="" className="flex flex-col">
-                        <input type="text" name="" placeholder="Enter your email" className="w-72 h-12 pl-2 border-b-2 border-b-slate-500 pr-3 my-5 mr-5 focus:border-b-slate-200 outline-0" />
-                        <button className="bg-green-500 w-28 410:pl-5 410:text-white h-10 md:ml-2 md:mt-2 rounded-2xl shadow-md shadow-black cursor-pointer">Subscribe</button>
+                        <input type="text" name="" placeholder="Enter your email" className={`${dancing.className} w-72 h-12 pl-2 border-b-2 border-b-slate-500 pr-3 my-5 mr-5 focus:border-b-slate-200 outline-0`} />
+                        <button className={`${cormorant.className} bg-green-500 w-28 320:mx-auto 370:mx-0 410:pl-5 410:text-white h-10 md:ml-2 md:mt-2 rounded-2xl shadow-md shadow-black cursor-pointer`}>Subscribe</button>
                     </form>
                 </div>
             </div>
