2413bb197c6b851063db76aed6a866299b0d793d
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-10-02 15:46:33 -0700

    Adding breakpoint styling to match fixed nav

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         //  Header start here
 
-    <div className="header-div flex my-3 320:h-700 320:flex-col 320:text-center 410:h-800 850:text-left 850:flex-row">
+    <div className="header-div flex my-3 320:h-800 320:pt-25 320:flex-col 320:text-center 410:h-800 850:text-left 850:flex-row">
       <div className="md:flex-row flex-1 h-full 850:pt-34">
         <h1 className={`320:text-3xl ${cormorant.className} 410:text-small-40 540:text-small-45 my-2 text-5xl pl-4`}>The Website for simple Monitoring of your social Activities across every known Social media</h1>
         <p className={`320:text-small-15 ${epunda.className} 410:text-small-17 410:w-4/5 mx-auto my-2 md:text-lg 850:w-full pl-4 font-semibold text-white`}>Activist dashboard help manage your activities and keep you well organize. <br className="320:hidden" /> we work with all known application and show real data by tracking all <br className="320:hidden" /> big events and notifying you of any important to your care.</p>
