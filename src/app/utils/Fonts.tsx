981127e2d7300774c64ac7d0568aa9cf486c7c86
Danieljeffery695 <danielgodfrey695@gmail.com>
2025-10-06 09:50:15 -0700

    Restructing my folder and layout

---
+++ src/app/utils/Fonts.tsx
@@ -0,0 +1,21 @@
+import localFont from 'next/font/local'
+ 
+export const cormorant = localFont({
+  src: '../../../public/fonts/Cormorant.ttf',
+}); 
+
+export const dancing = localFont({
+  src: "../../../public/fonts/Dancing.ttf",
+});
+
+export const epunda = localFont({
+  src: "../../../public/fonts/EpundaSlab.ttf",
+});
+
+export const karla = localFont({
+  src: "../../../public/fonts/Karla.ttf",
+});
+
+export const roboto = localFont({
+  src: "../../../public/fonts/Roboto.ttf",
+});
